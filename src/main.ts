b7704b26f2d98c3434a248f9b07810d26bd7326e
aegismichael4 <aegismichael4@gmail.com>
2025-10-17 02:28:50 +0000

    canvas set up !!!!! (stage 1 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,42 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
-  <h1>Sticker Sketchpad!!!</h1>
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <center><h1>Sticker Sketchpad!!!</h1><center>
+  <center><canvas id="canvasElement" width="256px" height="256px""></canvas></center>
+  <p>\n\n</p>
+  <center><button id="clear">clear</button></center>
+  <p id="test">test</p>
 `;
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+//#region HTML ELEMENTS
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+
+const canvas = document.getElementById("canvasElement")!;
+const test = document.getElementById("test")!;
+
+//#endregion
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+//#region MOUSE LOGIC
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+
+let mouseActive: boolean = false;
+
+canvas.addEventListener("mousedown", () => {
+  mouseActive = true;
+  test.innerHTML = "true";
+});
+
+canvas.addEventListener("mouseup", () => {
+  mouseActive = false;
+  test.innerHTML = "false";
+});
+
+canvas.addEventListener("mouseleave", () => {
+  mouseActive = false;
+  test.innerHTML = "false";
+});
+
+canvas.addEventListener("mousemove", () => {
+});
